38c2db2060ea5e53caaafc377767ef47549fe08a
Rahim_Badsa <rahimbadsa723@gmail.com>
2025-09-21 22:16:18 +0600

    login page with react hook form

--- apps/user-ui/src/app/(routes)/login/page.tsx
+++ apps/user-ui/src/app/(routes)/login/page.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import Link from "next/link";
 import GoogleButton from "./../../shared/components/buttons/google-button";
 import SectionDivider from "./../../shared/components/Divider/";
+import { Eye, EyeOff } from "lucide-react";
 
 type FormData = {
   email: string;
@@ -14,7 +15,7 @@ type FormData = {
 const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [serverError, setServerError] = useState<string | null>(null);
-  const [rememderMe, setRememberMe] = useState(false);
+  const [rememderMe, setRememberaMe] = useState(false);
   const router = useRouter();
 
   const {
@@ -23,7 +24,9 @@ const Login = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const onSubmit = (data: FormData) => {};
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
 
   return (
     <div className="w-full py-10 min-h-[86vh] bg-gray-200">
@@ -53,7 +56,73 @@ const Login = () => {
 
           {/* form */}
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" />
+            <label className="block mb-1 gray-700">Email</label>
+            <input
+              type="email"
+              className="w-full p-2 mb-1 border border-gray-300 rounded outline-0"
+              placeholder="support@trinet.com"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Please enter a valid email address",
+                },
+              })}
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+
+            {/* password */}
+            <label className="block mb-1 gray-700">Password</label>
+            <div className="relative">
+              <input
+                type={passwordVisible ? "text" : "password"}
+                className="w-full p-2 mb-1 border border-gray-300 rounded outline-0"
+                placeholder="support@trinet.com"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                })}
+              />
+              <button
+                type="button"
+                onClick={() => {
+                  setPasswordVisible(!passwordVisible);
+                }}
+                className="absolute inset-y-0 flex items-center text-gray-500 right-3"
+              >
+                {passwordVisible ? <Eye /> : <EyeOff />}
+              </button>
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+              <div className="flex items-center justify-between my-4">
+                <label className="flex items-center text-gray-600">
+                  <input
+                    type="checkbox"
+                    className="mr-2"
+                    checked={rememderMe}
+                    onChange={() => setRememberaMe(!rememderMe)}
+                  />
+                  Remember Me
+                </label>
+                <Link
+                  href={"/forgot-password"}
+                  className="text-sm text-blue-500"
+                >
+                  Forgot passoword
+                </Link>
+              </div>
+            </div>
+
+            <button className="w-full py-2 text-lg bg-black rounded cursor-pointer">
+              Login
+            </button>
+            {serverError && <p className="text-red-500">{serverError}</p>}
           </form>
         </div>
       </div>
